c4eb68bc7b9f49f85e1127a8179429970bb204bb
Skiiicodle <121371082+Skiiidoodle@users.noreply.github.com>
2024-07-03 13:19:09 +0700

    Update training_model.py

--- training_model.py
+++ training_model.py
@@ -1,8 +1,15 @@
 from neural_network import *
 import pickle
+import numpy as np
 
 # Fitting the model with the train set and run it for 100 iteration over the set
 W1, b1, W2, b2 = fit(X_train, Y_train, 0.1, 100)
+
+layer_1 = np.count_nonzero(W1) + np.count_nonzero(b1==0)
+layer_2 = np.count_nonzero(W2) + np.count_nonzero(b2==0)
+print(f"Layer 1 parameters: {layer_1}")
+print(f"Layer 2 parameters: {layer_2}")
+print(f"Total trainable parameters: {layer_1 + layer_2}\n")
 
 with open('parameters.pkl', 'wb') as f:
     # Use pickle.dump to serialize each variable and write them to the file
